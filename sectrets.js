75826e5d7da2256cc0c8db743c5e8eb4972d9ad1
Andy Stocchetti <ajstocchetti@gmail.com>
2016-03-25 13:48:26 -0400

    remove secrets

--- sectrets.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	skynetUrl: 'mongodb://SkynetReadWrite:zDyxbep7jxdmpcBc8pfjuLVY@10.0.11.134:27017,10.0.11.79:27017,10.0.10.216:27017/skynet?replicaSet=SamTheEagle&authSource=admin',
-	analyticsUrl: 'mongodb://Analytics:L3x1^gt0n@10.0.11.134:27017,10.0.11.79:27017,10.0.10.216:27017/skynet?replicaSet=SamTheEagle&authSource=admin'
-};
