d79b465471c48a24927aed703efe1cddc544eec4
Will Jacobson <will@wills-imac.internal.prescriptivedata.io>
2016-03-28 13:58:43 -0400

    messages posted through api

--- node/watcher.js
+++ node/watcher.js
@@ -31,12 +31,8 @@ function processFile(path) {
 
         json = JSON.parse(jsonStr); // this should be in try/catch block
 
-        // add to predictions
-        mongo.analyticsConnection()
-        .then(function(db) {
-            return mongo.insert(db, 'startup_prediction', jfixed, { 'checkKeys' : false });
-        }).then(function(db) { db.close(); })
-        .catch(function(err) { console.log("Error: ", err); })
+        // add to predictions collection in db
+        savePrediction(jfixed);
 
         // create message and add to messages
         create_save_message(json);
@@ -44,6 +40,14 @@ function processFile(path) {
         // archive file
         archiveFile(path, jsonStr);
     });
+}
+
+function savePrediction(json) {
+    mongo.analyticsConnection()
+        .then(function(db) {
+            return mongo.insert(db, 'startup_prediction', json, { 'checkKeys' : false });
+        }).then(function(db) { db.close(); })
+        .catch(function(err) { console.log("Error: ", err); });
 }
 
 function archiveFile(filePath, data) {
@@ -107,64 +111,65 @@ function create_save_message(json_data) {
         "fe_vis": true
     }
 
-    var messageAPI = {
-        "namespace": building,
-        "date": message_date,
-        "name": action,
-        "body": {
-            "score": json_data["345_Park"]["random_forest"]["best_start_time"]["score"],
-            "time-of-doc-generation" : rightNow,
-            "prediction-time" : startup_dt,
-            "method": "API"
-        },
-        "status": "cancel",
-        "time": startup_dt,
-        "type": "alert",
-        "fe_vis": true
-    }
-
-
     // Use node request library to post that message to https://byuldings.nantum.io/345_Park/messages
-    var options = {
-        url: 'https://buildings.nantum.io/345_Park/messages',
+    var headers = {
+        "authorization": "7McdaRC6fULlka2cPgsZ",
+        "version": "0.0.1",
+        "Content-Type": "application/json"
+    };
+    var msgPptions = {
+        "url": 'https://buildings.nantum.io/345_Park/messages',
         "method": "POST",
-        headers: {
-            "authorization": "7McdaRC6fULlka2cPgsZ",
-            "version": "0.0.1"
-        },
+        "headers": headers,
         "json": true,
         "body": messageAPI
-    }
+    };
 
     function cb(err, res, body) {
-        if (err) logger(err)
+        if (err) logger(err);
         if (!err && res.statusCode == 200) console.log(body);
     }
 
-    request(options, cb);
+    request(msgOptions, cb);
+
+    // update daily timeseries
+    var tsGetOptions = {
+        url: 'https://buildings.nantum.io/345_Park/messages',
+
+    }
+    function updateTs(toAppend) {
+
+        var tsOptions = {
+            url: 'https://buildings.nantum.io/345_Park/messages',
+            "method": "POST",
+            headers: headers,
+            "json": true,
+            "body": ts
+        }
+    }
 
 
     // Alternately, place message in db directly
-    mongo.skynetConnection()
-        .then(function(db) {
-            return mongo.update(db,
-                        'messages',
-                        { 'namespace' : building, 'name' : action, 'status' : 'pending', 'date' : message_date },
-                        { '$set' : { 'status' : 'cancel' }},
-                        { 'multi' : true })
-        }).then(function(db) {
-            return mongo.update(db,
-                        'messages',
-                        { 'namespace' : building, 'name' : action, 'status' : 'ack', 'date' : message_date },
-                        { '$set' : { 'status' : 'cancel' }},
-                        { 'multi' : true })
-        }).then(function(db) {
-            return mongo.insert(db, 'messages', message);
-        }).then(function(db) {
-            db.close();
-        }).then(function(db) {
-            console.log("done saving message");
-        }).catch(function(err) {
-            console.log("Error:", err);
-        })
+    // mongo.skynetConnection()
+    //     .then(function(db) {
+    //         return mongo.update(db,
+    //                     'messages',
+    //                     { 'namespace' : building, 'name' : action, 'status' : 'pending', 'date' : message_date },
+    //                     { '$set' : { 'status' : 'cancel' }},
+    //                     { 'multi' : true })
+    //     }).then(function(db) {
+    //         return mongo.update(db,
+    //                     'messages',
+    //                     { 'namespace' : building, 'name' : action, 'status' : 'ack', 'date' : message_date },
+    //                     { '$set' : { 'status' : 'cancel' }},
+    //                     { 'multi' : true })
+    //     }).then(function(db) {
+    //         return mongo.insert(db, 'messages', message);
+    //     }).then(function(db) {
+    //         db.close();
+    //     }).then(function(db) {
+    //         console.log("done saving message");
+    //     }).catch(function(err) {
+    //         console.log("Error:", err);
+    //     })
 }
