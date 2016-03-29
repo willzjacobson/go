83a05a31251e9f4f1b9555c9a66262d80733cf04
William Jacobson <williamjacobson@Williams-MacBook-Pro.local>
2016-03-29 01:10:19 -0400

    modularized API calls

--- node/temp.js
+++ node/temp.js
@@ -27,75 +27,27 @@ function create_save_message(json_data) {
         "fe_vis": true
     };
 
-    // Use node request library to update the message for this prediction day, or post one if one doesn't exits
-    var query = {
-        "name": "morning-startup",
-        "date": message_date,
-        "namespace": building
-    };
-    var qs = new Buffer( JSON.stringify( query ) ).toString('base64');
-
-
-
-    var headers = {
-        "authorization": "7McdaRC6fULlka2cPgsZ",
-        "version": "0.0.1",
-        "Content-Type": "application/json"
-    };
-    var getMessageOptions = {
-        "url": "https://buildings.nantum.io/" + building + "/messages/?q=" + qs,
-        "headers": headers
-    };
-
-
-    // Do the request
-    request(getMessageOptions, function(err, res, body) {
-
-        var options = {
-            "json": true,
-            "headers": headers,
-            "body": message
-        };
-
-        if (err) logger("error GETting messages: " + err);
-
-        else if (res.statusCode == 200 && body.docs) {
-            // If there are more than 0 messages for the day that the prediction is for
-
-            options.method = "PUT";
-            options.url = "https://buildings.nantum.io/" + building + "/messages/" + body.docs[0]._id;
-
-            request(options, function(err, res, body) {
-                if (err) logger("error PUTing message: " + err.toString());
-                else console.log("Success PUTing message: " + body.toString());
-            });
-
-        } else if (res.statusCode == 200) {
-            // If there are not yet messages for the day the prediction is for
-
-            options.method = "POST";
-            options.url = "https://buildings.nantum.io/" + building + "/messages";
-
-            request(options, function(err, res, body) {
-                if (err) logger("error POSTing message: " + err.toString());
-                else console.log("Success POSTing message: " + body.toString());
-            });
-        }
-    });
-
+    create_or_update("messages", building, message);
 }
 
 
 
-var query = {
-    "type" : type,
-    "target" : target
-};
-query = new Buffer(JSON.stringify(query)).toString('base64');
 
+function create_or_update(resource, namespace, obj, target, type, state) {
+    
+    // Build query string
+    var query = {};
+    if (resource == "states") {
+        query.type = type;
+        query.target = target
+    } else if (resource == "messages") {
+        query.name = "morning-startup";
+        query.date = message_date;
+        query.namespace = namespace
+    }
+    query = new Buffer(JSON.stringify(query)).toString('base64');
 
-function updateStuff(resource, query, obj, namespace, target, type, state) {
-    // make GET request to find ID
+    // Define headers and options for request
     var headers = {
         'authorization' : '7McdaRC6fULlka2cPgsZ',
         'version' : '0.0.1',
@@ -105,30 +57,33 @@ function updateStuff(resource, query, obj, namespace, target, type, state) {
         url : "https://buildings.nantum.io/" + namespace + "/" + resource + "?q=" + query,
         headers : headers
     };
+
+    // Make get request to find id if document exists
     request(options, function(err, response, body) {
         if(err || response.statusCode != 200) {
-            logger("Error lookup up state for building/target/type: " + namespace + "/" + target + "/" + type);
+            if (resource == "states") logger("Error lookup up " + resource + " for building/target/type: " + namespace + "/" + target + "/" + type);
+            if (resource == "messages") logger("Error lookup up " + resource + " for building/name/date: " + namespace + "/" + obj.name + "/" + obj.message_date);
             logger(err);
             return;
         }
 
-        // BUILD OBJECT TO PUT
+        // Build object to PUT/POST
         var now = new Date();
         var update = { "obj" : {} };
-        if (resource == "state") {
+        if (resource == "states") {
             update.obj.namespace = namespace;
             update.obj.type = type;
             update.obj.state = state;
             update.obj.last_modified = now;
             update.obj.last_modified_by = "an_go filewatcher";
             if(target) { update.obj.target = target; }
-        } else if (resource == "morning-startup" ) {
+        } else if (resource == "messages" ) {
             update.obj = obj;
         }
 
+        // Define options for update/save
         var options = { 
-            method : 'PUT',
-            url : "https://buildings.nantum.io/" + namespace + "/resource/",
+            url : "https://buildings.nantum.io/" + namespace + "/" + resource + "/",
             headers : headers,
             json : true,
             body : update
@@ -140,13 +95,18 @@ function updateStuff(resource, query, obj, namespace, target, type, state) {
         } else { // no document exists, create one with POST
             options.method = 'POST';
         }
+
+        // Make the update/save
         request(options, function(err, response, body) {
             if(err) {
-                logger("Error saving " +  resource + " for building/target/type: " + namespace + "/" + target + "/" + type);
+                if (resource == "states") logger("Error saving " +  resource + " for building/target/type: " + namespace + "/" + target + "/" + type);
+                if (resource == "messages") logger("Error saving " + resource + " for building/name/date: " + namespace + "/" + obj.name + "/" + obj.message_date);
                 logger(err);
                 return;
+            } else {
+                if (resource == "states") logger("Successful save/update: " +  resource + " for building/target/type: " + namespace + "/" + target + "/" + type);
+                if (resource == "messages") logger("Successful save/update: " + resource + " for building/name/date: " + namespace + "/" + obj.name + "/" + obj.message_date);
             }
-            // TODO: check for OK status code
         });
     });
 }
