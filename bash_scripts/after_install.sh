1cf0d7da5e325482da5fb57f46bd2bfcf1fa8fa8
Will Jacobson <will@wills-imac.internal.prescriptivedata.io>
2016-04-05 16:41:29 -0400

    yay

--- bash_scripts/after_install.sh
+++ bash_scripts/after_install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 mkdir /var/analytics/FUN/yuck
 cd /var/analytics/an_go/node
-touch .env
-echo "AWS_S3_JSON_ARCHIVE_BUCKET=pdanalytics" >> .env
-echo "MONGO_SKYNET_URL=mongodb://SkynetReadWrite:zDyxbep7jxdmpcBc8pfjuLVY@10.0.11.134:27017,10.0.11.79:27017,10.0.10.216:27017/skynet?replicaSet=SamTheEagle&authSource=admin" >> .env
-echo "MONGO_ANALYTICS_URL=mongodb://Analytics:L3x1^gt0n@10.0.11.134:27017,10.0.11.79:27017,10.0.10.216:27017/skynet?replicaSet=SamTheEagle&authSource=admin" >> .env
-npm install
+
+# s3 bucket name and db login info
+aws s3 cp s3://pdanalytics/.env .env
+
 mv /var/analytics/an_go/an_go.conf /etc/init/an_go.conf
+
+npm install
