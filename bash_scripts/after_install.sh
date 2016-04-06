34f5bd1fe80e3d6e5e9f3b129206dcc844328417
William Jacobson <williamjacobson@Williams-MacBook-Pro.local>
2016-04-05 22:42:05 -0400

    hoorrrray

--- bash_scripts/after_install.sh
+++ bash_scripts/after_install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-mkdir /var/analytics/FUN/yuck
-cd /var/analytics/an_go/node
 
 # s3 bucket name and db login info
-aws s3 cp s3://pdanalytics/.env .env
+aws s3 cp s3://pdanalytics/.env /var/analytics/an_go/node/.env
 
 mv /var/analytics/an_go/an_go.conf /etc/init/an_go.conf
 
+cd /var/analytics/an_go/node
 npm install
 
 reboot
